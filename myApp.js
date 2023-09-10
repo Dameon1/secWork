54775771c801e572a979d7c1a941571a975dfc3c
Dameon Mendoza <dameonmendoza@gmail.com>
2023-09-10 06:47:27 -0700

    hsts

--- myApp.js
+++ myApp.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
+const timeInSeconds =90*24*60*60; 
 
+
+
+helmet.hsts({maxAge: timeInSeconds, force: true});
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen())
+app.use(helmet.ieNoOpen());
 
 
 
