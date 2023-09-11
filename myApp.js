d73d5bc69da0a8829bf996d5e4909755a5a251a0
Dameon Mendoza <dameonmendoza@gmail.com>
2023-09-11 07:44:21 -0700

    preFetch controller

--- myApp.js
+++ myApp.js
@@ -5,13 +5,13 @@ const timeInSeconds =90*24*60*60;
 
 
 
+app.use(helmet.dnsPrefetchController());
 app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
-//app.use(helmet.dnsPrefetchController());
 
 
 
